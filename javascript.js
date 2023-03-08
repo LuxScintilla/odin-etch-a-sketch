53a3fe12a6d950a64775cc5d994b800300927604
LuxScintilla <catherina.ea@gmail.com>
2023-03-08 23:50:51 +0000

    Add code to menu items

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 const gridContainer = document.querySelector(".canvas");
 const buttonNew = document.querySelector("#menu-new");
-const slider = document.querySelector(".slider");
-let sliderOutput = document.querySelector(".slider-output");
+const inputBox = document.querySelector(".input-box");
 const buttonClear = document.querySelector("#menu-clear");
+const buttonToggle = document.querySelector("#menu-toggle");
 const pencil = document.querySelector(".pencil");
 const pencilRainbow = document.querySelector(".pencil-rainbow");
 const fill = document.querySelector(".fill");
@@ -14,6 +14,7 @@ let presetColor = document.querySelectorAll(".preset");
 let chosenPixels;
 let canvas;
 
+
 function defaultGrid() {
     for(i = 0; i < 256; i++) {
         const gridElement = document.createElement("div");
@@ -22,36 +23,95 @@ function defaultGrid() {
 }
 defaultGrid()
 
-slider.oninput = function () {
-    sliderOutput.textContent = this.value;
-}
 
-let allBlocks = document.querySelectorAll(".grid-item");
-for(let block of allBlocks) {
+buttonNew.addEventListener("click", function() {
 
- 
-    block.addEventListener("mousemove", draw);
-
-    function draw(e) {
-        if(e.buttons !== 1) {
-            return;
-        } else {
-            block.classList.remove("grid-item");
-            block.classList.add("grid-item-colored");
-        }
-
+    while(gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.lastChild);
     }
 
+    chosenPixels = inputBox.value;
 
+    canvas = chosenPixels * chosenPixels;
+
+    gridContainer.style.gridTemplateColumns = `repeat(${chosenPixels}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${chosenPixels}, 1fr)`;
+
+    for(i = 0; i < canvas; i++) {
+        const gridElement = document.createElement("div");
+        gridContainer.appendChild(gridElement).classList.add("grid-item");
+    }
+
+});
+
+
+let allBlocks = document.querySelectorAll(".grid-item");
+
+
+buttonClear.addEventListener("click", function() {
+    for(let block of allBlocks) {
+        block.classList.remove("grid-item-colored");
+        block.classList.add("grid-item");
+    }
+});
+
+
+
+buttonToggle.addEventListener("click", function() {
+    gridContainer.classList.toggle("toggle-grid");
+});
+
+
+
+for(let block of allBlocks) {
+    block.addEventListener("mousedown", mouseDown);
+    block.addEventListener("mousemove", mouseMove);
+
+    function mouseDown(e) {
+        if(e.buttons !== 1) {
+            return;
+        } else if(e.buttons == 1) {
+        block.classList.remove("grid-item");
+        block.classList.add("grid-item-colored");
+        }
+    }
+    function mouseMove(e) {
+        if(e.buttons !== 1) {
+            return;
+        } else if(e.buttons == 1) {
+        block.classList.remove("grid-item");
+        block.classList.add("grid-item-colored");
+        }
+    }
 }
 
+
+
+
+
+
+
+// !!!!! THIS IS WORKING WITH CLICK AND DRAG !!!!!
+
+// for(let block of allBlocks) {
+//     block.addEventListener("mousemove", draw);
+
+//     function draw(e) {
+//         if(e.buttons !== 1) {
+//             return;
+//         } else {
+//             block.classList.remove("grid-item");
+//             block.classList.add("grid-item-colored");
+//         }
+//     }
+// }
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 
 // !!!!! THIS IS WORKING WITH HOVER !!!!!
 
-
-// let allBlocks = document.querySelectorAll(".grid-item");
 // for(let block of allBlocks) {
 
 //     block.addEventListener("mouseover", function() { 
@@ -61,38 +121,3 @@ for(let block of allBlocks) {
 // }
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
-
-
-
-// buttonNew.addEventListener("click", function() {
-//     chosenPixels = prompt("Give me a number, I give you a canvas.")
-
-//     canvas = chosenPixels * chosenPixels;
-
-//     while(gridContainer.hasChildNodes()) {
-//         gridContainer.removeChild(gridContainer.lastChild);
-//     }
-//     createGrid()
-    
-//     gridContainer.style.gridTemplateColumns = `repeat(${chosenPixels}, 1fr)`;
-//     gridContainer.style.gridTemplateRows = `repeat(${chosenPixels}, 1fr)`;
- 
-// });
-
-// function createGrid() {
-//     for(i = 0; i < canvas; i++) {
-//         const gridElement = document.createElement("div");
-//         gridContainer.appendChild(gridElement).classList.add("grid-item");
-//     }
-// }
-
-// let allBlocks = document.querySelectorAll(".grid-item");
-// for(let block of allBlocks) {
-//     block.addEventListener("mouseover", function() {
-//         block.classList.remove("grid-item");
-//         block.classList.add("grid-item-colored");
-//     });
-// }
