d76611e5af22a69ee8a0a1e95bd0a5a30f344035
LuxScintilla <catherina.ea@gmail.com>
2023-03-09 23:01:55 +0000

    Switch between 2 tools & get preset values

--- javascript.js
+++ javascript.js
@@ -9,9 +9,42 @@ const fill = document.querySelector(".fill");
 const droplet = document.querySelector(".droplet");
 const eraser = document.querySelector(".eraser");
 const colorPicker = document.querySelector(".color-picker");
+let allPresetColors = document.querySelector(".preset-colors");
 let presetColor = document.querySelectorAll(".preset");
 
-// INPUT CANVAS SIZE
+// TOOLS AND PRESET COLORS-------------------------------
+
+let chosenColor;
+
+pencil.addEventListener("click", function() {
+    pencil.style.border = "2px solid red";
+    pencilRainbow.style.border = "";
+
+    chosenColor = "#000000";
+});
+
+pencilRainbow.addEventListener("click", function() {
+    pencilRainbow.style.border = "2px solid red";
+    pencil.style.border = "";
+    
+    rainbowArray = ["#fb0304", "#fc8044", "#fefe04", "#02fd02", "#0201fa", "#ff01fd"];
+    random = Math.floor(Math.random() * rainbowArray.length);
+
+    chosenColor = `${rainbowArray[random]}`;
+});
+
+
+
+
+
+for(let color of presetColor) {
+    color.addEventListener("click", function() {
+        console.log(this.dataset.value);
+    });
+}
+
+
+// INPUT CANVAS SIZE-------------------------------------
 let chosenPixels;
 let canvas;
 function myCanvas(input) {
@@ -22,7 +55,40 @@ function myCanvas(input) {
     }
 }
 
-// NEW CANVAS
+// CREATE CANVAS-----------------------------------------
+function createCanvas() {
+    if(chosenPixels <= 64) {
+        for(i = 0; i < canvas; i++) {
+            const gridBlock = document.createElement("div");
+            gridContainer.appendChild(gridBlock).classList.add("grid-item");
+            gridBlock.addEventListener("mousedown", function(e) {
+                if(e.buttons !== 1) {
+                    return;
+                } else if(e.buttons == 1) {
+                    gridBlock.style.backgroundColor = chosenColor;
+                }
+            });
+            gridBlock.addEventListener("mousemove", function(e) {
+                if(e.buttons !== 1) {
+                    return;
+                } else if(e.buttons == 1) {
+                    gridBlock.style.backgroundColor = chosenColor;
+                }
+            });
+        }
+    } else {
+        while(gridContainer.hasChildNodes()) {
+            gridContainer.removeChild(gridContainer.lastChild);
+        }
+
+        const para = document.createElement("p");
+        const text = document.createTextNode("Try again 1-64");
+        para.appendChild(text);
+        gridContainer.appendChild(para).classList.add("howto");
+    }
+}
+
+// NEW CANVAS BUTTON-----------------------------------
 buttonNew.addEventListener("click", function() {    
 
     while(gridContainer.hasChildNodes()) {
@@ -35,34 +101,10 @@ buttonNew.addEventListener("click", function() {
     gridContainer.style.gridTemplateColumns = `repeat(${chosenPixels}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${chosenPixels}, 1fr)`;
 
-    if(chosenPixels <= 64) {
-        for(i = 0; i < canvas; i++) {
-            const gridBlock = document.createElement("div");
-            gridContainer.appendChild(gridBlock).classList.add("grid-item");
-            gridBlock.addEventListener("mousedown", function(e) {
-                if(e.buttons !== 1) {
-                    return;
-                } else if(e.buttons == 1) {
-                    gridBlock.style.backgroundColor = "#000000";
-                }
-            });
-            gridBlock.addEventListener("mousemove", function(e) {
-                if(e.buttons !== 1) {
-                    return;
-                } else if(e.buttons == 1) {
-                    gridBlock.style.backgroundColor = "#000000";
-                }
-            });
-        }
-    } else {
-        while(gridContainer.hasChildNodes()) {
-            gridContainer.removeChild(gridContainer.lastChild);
-        }
-    }
-
+    createCanvas()
 });
 
-// CLEAR THE CANVAS
+// CLEAR THE CANVAS BUTTON-----------------------------
 buttonClear.addEventListener("click", function() {
     const gridBlocks = document.querySelectorAll(".grid-item");
     for(let block of gridBlocks) {
@@ -70,45 +112,7 @@ buttonClear.addEventListener("click", function() {
     }
 });
 
-// TOGGLE THE GRID
+// TOGGLE THE GRID-------------------------------------
 buttonToggle.addEventListener("click", function() {
     gridContainer.classList.toggle("toggle-grid");
 });
-
-
-
-
-
-
-
-
-// !!!!! THIS IS WORKING WITH CLICK AND DRAG !!!!!
-
-// for(let block of allBlocks) {
-//     block.addEventListener("mousemove", draw);
-
-//     function draw(e) {
-//         if(e.buttons !== 1) {
-//             return;
-//         } else {
-//             block.classList.remove("grid-item");
-//             block.classList.add("grid-item-colored");
-//         }
-//     }
-// }
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
-// !!!!! THIS IS WORKING WITH HOVER !!!!!
-
-// for(let block of allBlocks) {
-
-//     block.addEventListener("mouseover", function() { 
-//         block.classList.remove("grid-item");
-//         block.classList.add("grid-item-colored");
-//     });
-// }
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
